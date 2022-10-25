e56bfce0f632c8e2c471c9d0bcb3f381b08ce83a
ryota050505 <05ryouta05@gmail.com>
2022-10-25 23:50:08 +0900

    :+1: Flyway migration & Thymeleaf 疎通完了

--- src/main/java/com/example/coco/controller/IndexController.java
+++ src/main/java/com/example/coco/controller/IndexController.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
-    @GetMapping
-    public ModelAndView index() {
-        final var modelAndView = new ModelAndView("index");
-        return modelAndView;
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
